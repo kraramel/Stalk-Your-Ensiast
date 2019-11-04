7aee9b326319d6f1be1400cb25a61cf254af342c
Amin Asri <asri.med.amine@gmail.com>
2019-11-04 14:06:05 +0100

    Updates

---
+++ precache-manifest.b872e0b56b3a677c65c7b785d318e29d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e2697c65f7c7b235e528ab67e87460b",
+    "url": "/Stalk-Your-Ensiast/index.html"
+  },
+  {
+    "revision": "36e3650ae3d13ac5d2fa",
+    "url": "/Stalk-Your-Ensiast/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "64526dee1a7ee4297f7f",
+    "url": "/Stalk-Your-Ensiast/static/css/main.29ba3b96.chunk.css"
+  },
+  {
+    "revision": "36e3650ae3d13ac5d2fa",
+    "url": "/Stalk-Your-Ensiast/static/js/2.195cf6c1.chunk.js"
+  },
+  {
+    "revision": "64526dee1a7ee4297f7f",
+    "url": "/Stalk-Your-Ensiast/static/js/main.858fa7c9.chunk.js"
+  },
+  {
+    "revision": "0f0f7c6b42894705b5da",
+    "url": "/Stalk-Your-Ensiast/static/js/runtime-main.3f08a2a5.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Stalk-Your-Ensiast/static/media/SEGA.12a3db8b.woff"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Stalk-Your-Ensiast/precache-manifest.b872e0b56b3a677c65c7b785d318e29d.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Stalk-Your-Ensiast/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
